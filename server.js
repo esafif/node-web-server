e7cc736eca144485b6ddb5ad443e11f87ac216e6
esafif <esafif637@gmail.com>
2017-12-01 21:24:04 +0700

    update again 9.23

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const hbs = require('hbs');
-
+const fs = require('fs');
 
 var app = express();
 
@@ -8,6 +8,22 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('Server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable append to server log');
+        }
+    });
+    next();
+});
+
+app.use((re, res, next) => {
+    res.render('maintenance.hbs');
+});
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
