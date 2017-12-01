dfd88033ce6e2466328d03b223556f3cb6695fd7
esafif <esafif637@gmail.com>
2017-12-01 20:22:20 +0700

    add hbs template engine

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const express = require(`express`);
+const hbs = require('hbs');
+
 
 var app = express();
 
+app.set('view engine','hbs');
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     //res.send('<h1>hello express safif</h1>');
-    res.send({
-        name: 'safif',
-        likes: [
-            'coding',
-            'programming'
-        ]
+    // res.send({
+    //     name: 'safif',
+    //     likes: [
+    //         'coding',
+    //         'programming'
+    //     ]
+    // });
+
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        wellcomeMessagae: 'Selamat datang safif',
+        currentYear: new Date().getFullYear()
     });
+
 });
 
 app.get('/about', (req, res) =>{
-    res.send("About Page");
+    res.render('about.hbs', {
+        pageTitle: 'Safif',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) =>{
