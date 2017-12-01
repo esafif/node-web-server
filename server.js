2b0398b9eee596a0ae2fbd6bf91764f6bb9d7b4f
esafif <esafif637@gmail.com>
2017-12-01 17:27:07 +0700

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = reuire(`express`);
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello express');
+    res.send('hello express safif');
 });
 
 app.listen(3000);
