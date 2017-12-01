70380ed45eadc206983fc1a59c6cc82fe99aea4f
esafif <esafif637@gmail.com>
2017-12-01 17:56:22 +0700

    change server.js

--- server.js
+++ server.js
@@ -3,7 +3,24 @@ const express = require(`express`);
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello express safif');
+    //res.send('<h1>hello express safif</h1>');
+    res.send({
+        name: 'safif',
+        likes: [
+            'coding',
+            'programming'
+        ]
+    });
+});
+
+app.get('/about', (req, res) =>{
+    res.send("About Page");
+});
+
+app.get('/bad', (req, res) =>{
+    res.send({
+        errorMessage: "Unable to handle request"
+    });
 });
 
 app.listen(3000);
